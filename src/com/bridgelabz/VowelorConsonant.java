74a90a7c3017981c3ff654fb65810c1c84541aa0
manishareddymuplam <manishamuplam@gmail.com>
2022-04-28 02:16:59 -0700

    added:vowelorconsonant

---
+++ src/com/bridgelabz/VowelorConsonant.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class VowelorConsonant {
+        public static void main(String[] arge) {
+
+            char ch = 'w';
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                System.out.println(ch + " is vowel");
+            else
+                System.out.println(ch + " is consonant");
+        }
+    }
+
+
